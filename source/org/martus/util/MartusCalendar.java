7ff4ebfd3784d61fed78c90cfe9a1aebf67d3287
kevins <kevins@localhost>
2005-09-23 19:34:51 +0000

    Replace almost all GregorianCalendar objects with MartusCalendar objects

---
+++ source/org/martus/util/MartusCalendar.java
@@ -0,0 +1,126 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2005, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+
+package org.martus.util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.SimpleTimeZone;
+
+public class MartusCalendar
+{
+	public MartusCalendar()
+	{
+		cal = new GregorianCalendar(new SimpleTimeZone(UTC_OFFSET, "martus"));
+		clearTimeOfDay();
+	}
+	
+	public MartusCalendar(int year, int month, int day)
+	{
+		this();
+		set(year, month, day);
+	}
+	
+	public MartusCalendar(Calendar copyFrom)
+	{
+		this(copyFrom.get(Calendar.YEAR), copyFrom.get(Calendar.MONTH), copyFrom.get(Calendar.DAY_OF_MONTH));
+	}
+	
+	public MartusCalendar(MartusCalendar copyFrom)
+	{
+		this(copyFrom.get(Calendar.YEAR), copyFrom.get(Calendar.MONTH), copyFrom.get(Calendar.DAY_OF_MONTH));
+	}
+	
+	public int get(int field)
+	{
+		return cal.get(field);
+	}
+	
+	public void set(int field, int value)
+	{
+		cal.set(field, value);
+	}
+	
+	public void set(int year, int month, int day)
+	{
+		set(Calendar.YEAR, year);
+		set(Calendar.MONTH, month);
+		set(Calendar.DAY_OF_MONTH, day);
+	}
+	
+	public void add(int field, int value)
+	{
+		cal.add(field, value);
+	}
+	
+	public boolean before(MartusCalendar other)
+	{
+		return cal.before(other.cal);
+	}
+	
+	public boolean after(MartusCalendar other)
+	{
+		return cal.after(other.cal);
+	}
+	
+	public Date getTime()
+	{
+		return cal.getTime();
+	}
+	
+	public void setTime(Date newTime)
+	{
+		if(newTime.getTime() < 0)
+			newTime = new Date(0);
+		cal.setTime(newTime);
+	}
+
+	public long getTimeInMillis()
+	{
+		return cal.getTimeInMillis();
+	}
+	
+	public void setTimeInMillis(long millis)
+	{
+		cal.setTimeInMillis(millis);
+	}
+	
+	public Calendar getCalendar()
+	{
+		return cal;
+	}
+	
+	private void clearTimeOfDay()
+	{
+		set(Calendar.HOUR_OF_DAY, 12);
+		set(Calendar.MINUTE, 0);
+		set(Calendar.SECOND, 0);
+	}
+	
+	private static int UTC_OFFSET = 0;
+	private Calendar cal;
+}
