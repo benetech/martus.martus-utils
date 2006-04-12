c34cf6712415c20d4430f4021c9a78859fb1bee5
kevins <kevins@localhost>
2006-04-12 22:36:29 +0000

    Add class that can exclusive lock a directory

---
+++ source/org/martus/util/DirectoryLock.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.martus.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileLock;
+
+public class DirectoryLock
+{
+	public static class AlreadyLockedException extends Exception {}
+
+	public void lock(File directory) throws IOException, AlreadyLockedException
+	{
+		File lockFile = new File(directory, "lock");
+		FileOutputStream tempLockStream = new FileOutputStream(lockFile);
+		FileLock lock = tempLockStream.getChannel().tryLock();
+		if(lock == null)
+			throw new AlreadyLockedException();
+		lockStream = tempLockStream;
+	}
+	
+	public void close() throws IOException
+	{
+		if(lockStream == null)
+			return;
+		
+		FileOutputStream tempLockStream = lockStream;
+		lockStream = null;
+		tempLockStream.close();
+	}
+	
+	public boolean isLocked()
+	{
+		return (lockStream != null);
+	}
+	
+	private FileOutputStream lockStream;
+}
