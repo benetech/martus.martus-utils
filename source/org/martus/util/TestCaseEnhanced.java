e3c62be5046ff90a1b84760f716b9c0e6eaccd2f
charles <charles@localhost>
2006-04-19 19:10:41 +0000

    Speedup: BulletinFolder rawUidList is a Vector instead of a Set moved Vector conversion from Set to a utility function.

--- source/org/martus/util/TestCaseEnhanced.java
+++ source/org/martus/util/TestCaseEnhanced.java
@@ -32,10 +32,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
 import java.util.Vector;
-
 import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 import sun.reflect.Reflection;
@@ -105,18 +102,6 @@ public class TestCaseEnhanced extends TestCase
 		out.close();
 	}
 	
-	public Vector toVector(Set s)
-	{
-		Vector v = new Vector();
-		for(Iterator iter = s.iterator(); iter.hasNext();)
-		{
-			Object element = iter.next();
-			v.add(element);
-		}
-		return v;
-	}
-
-
 	public static void assertFalse(boolean actual)
 	{
 		if(actual)
